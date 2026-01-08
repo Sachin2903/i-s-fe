2cc1864b86260ae7d82aa2fa04bcc3191dce8083
sachin chaturvedim <sachinchaturvedi36952@gmail.com>
2026-01-08 18:14:36 +0530

    added tag script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NextTopLoader from "nextjs-toploader";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,18 @@ export default function RootLayout({
         <style>
           @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
         </style>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZY8WNV9C3Y"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZY8WNV9C3Y');
+          `}
+        </Script>
       </head>
 
       <body
--- src/component/landingPage/footer.tsx
+++ src/component/landingPage/footer.tsx
@@ -6,10 +6,10 @@ export default function Footer() {
         <aside className="flex md:max-w-[50%] w-full justify-start items-center md:items-start flex-col gap-2 md:gap-4">
             <Link href="/"> <img src={logo.src} alt="i&s" className="h-[40px]  md:h-[45px] lg:h-[55.37px]" /></Link>
             <p className="md:text-[17px]  lg:text-[18px] md:w-full max-w-[550px]  text-center md:text-start font-normal text-[#7F848D] text-[16px]">Leveraging on the facilities, technology and in depth knowledge about the different products such as pressure transmitters and indicator...</p>
-            <img src={india.src} alt="madeinindia" className="h-[63px]  md:h-[70px] lg:h-[77px]" />
+            <img src={india.src} alt="madeinindia" className="h-[63px]  my-5 md:h-[70px] lg:h-[77px]" />
         </aside>
         <aside className="w-full mt-10 md:mt-0 flex gap-[3%] lg:gap-[8%] justify-evenly md:justify-center items-start">
-            <ul className="flex flex-shrink-0 flex-col text-white justify-start  text-sm md:text-base font-medium items-start gap-2 lg:gap-3">
+            <ul className="flex  flex-col text-white justify-start  text-sm md:text-base font-medium items-start gap-2 lg:gap-3">
                 <Link href="/"><li>HOME</li></Link>
                 <Link href="/products"><li>PRODUCTS</li></Link>
                 <Link href="/industry"><li>INDUSTRY</li></Link>
@@ -17,7 +17,7 @@ export default function Footer() {
                 <Link href="/aboutus"><li>ABOUT US</li></Link>
                 <Link href="/contactus"><li>CONTACT US</li></Link>
             </ul>
-            <ul className="flex flex-shrink-0 flex-col text-white justify-start  text-sm md:text-base font-medium items-start gap-2 lg:gap-3">
+            <ul className="flex  flex-col text-white justify-start  text-sm md:text-base font-medium items-start gap-2 lg:gap-3">
                 <li>Product Category</li>
                 <Link href="/products?type=CONTROL VALVES"><li className="text-[#7F848D]">Control valves</li></Link>
                 <Link href="/products?type=TRANSMITTERS"><li className="text-[#7F848D]">Transmitters</li></Link>
